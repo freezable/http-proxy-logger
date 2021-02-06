42b8c8b9863ba409869903eba2fd9c0ba2bf7eac
yehor <yehor.chernyshov@kapten-son.com>
2021-02-06 21:38:55 +0100

    dyn endpoints

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
-const morganBody = require("morgan-body")
 const bodyParser = require('body-parser')
-const path = require('path')
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 require('dotenv').config()
-
 const port = process.env.PORT || 3000
+const indexFilePath =  require('path').join(__dirname + '/public/index.html')
 const healthStatusRouter = require('./routers/health-status')
 const monitorRouter = require('./routers/monitor')
 const apiRouter = require('./routers/api')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-
-const loggerStream = {
-    write: message => {
-        io.emit('request', message)
-    },
-}
-
-morganBody(app, {
-    logAllReqHeader: true,
-    logResponseBody: true,
-    maxBodyLength: 10000000,
-    noColors: true,
-    stream: loggerStream,
-    includeNewLine: true
-})
 
 const servers = [
     {
@@ -37,9 +20,8 @@ const servers = [
 ]
 
 app.use('/health-status', new healthStatusRouter().get())
-app.use('/monitor', new monitorRouter(process.env.MONITOR_AUTH_KEY, path.join(__dirname + '/public/index.html')).get())
-app.use('/api', new apiRouter(servers, 'api').get())
-
+app.use('/monitor', new monitorRouter(process.env.MONITOR_AUTH_KEY,indexFilePath).get())
+app.use('/api', new apiRouter(servers, 'api', io).get())
 
 http.listen(port, () => {
     console.log(`Monitoring server running at http://localhost:${port}/`)
--- routers/api.js
+++ routers/api.js
@@ -3,19 +3,34 @@ const {createProxyMiddleware} = require('http-proxy-middleware')
 
 module.exports = class ApiRouter {
 
-    constructor(servers, routerName) {
+    constructor(servers, routerName, io) {
         this.servers = servers
         this.routerName = routerName
+        this.io = io
     }
 
     get() {
+        let io = this.io;
         for (const server of this.servers) {
+            let fullEndpointName = '/' + this.routerName + server.routerEndpoint
             router.use(server.routerEndpoint, createProxyMiddleware({
                 target: server.target,
                 changeOrigin: true,
                 pathRewrite: {
-                    [`^/` + this.routerName + server.routerEndpoint]: '',
-                }
+                    [`^` + fullEndpointName]: '',
+                },
+                onProxyReq: (proxyReq, req, res) => {
+                    let dump = {
+                        timestamp: new Date().toLocaleString(),
+                        method: req.method,
+                        url: req.originalUrl.replace(fullEndpointName, server.target),
+                        headers: req.headers,
+                        params: req.params,
+                        body: req.body
+                    }
+                    io.emit(new URL(server.target).hostname, dump)
+                    io.emit('request', dump)
+                },
             }))
         }
 
